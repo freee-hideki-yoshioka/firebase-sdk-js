46879025f39332b9adc4438e2817941452cc92be
Hideki Yoshioka <hideki-yoshioka@c-fo.com>
2021-02-08 22:18:09 +0900

    テスト

--- src/sdk/services/token-manager.ts
+++ src/sdk/services/token-manager.ts
@@ -15,12 +15,14 @@ export class TokenManager {
     this.oauth2 = oauth2
     this.cryptor = cryptor
     this.tokenCache = {}
+    console.log({constructor_tokenCache: this.tokenCache})
   }
 
   /**
    * Get token with handling refresh token
    */
   async get(userId: string): Promise<string> {
+    console.log({get_tokenCache: this.tokenCache})
     let freeeToken: any;
 
     try {
